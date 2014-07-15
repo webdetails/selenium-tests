8b14aa17bcd29f37f594ca918a28e4c9a7a014c5
Emanuel Pedro Loureiro Teixeira <emanuel.teixeira@webdetails.pt>
2014-07-15 11:21:42 +0100

    Update test case with waitFor.

--- run-html-tests.bat
+++ run-html-tests.bat
@@ -1,10 +1,17 @@
 echo ON
 
+echo param1:%1
+echo param2:%2
+echo param3:%3
+echo param4:%4
+
+
+
 set param.lib.selenium.standalone=lib\selenium-server-standalone.jar
 set param.browser=*firefox
 set param.host=http://localhost:8080/pentaho/
 set param.testsuite=src-html\test_suite_global_jenkins.html
 set param.testsuite.results=results.html
-set param.options=-timeout 60000
+set param.options=-timeout 60000 -browserTimeout 60000
 
 java -jar %param.lib.selenium.standalone% -htmlSuite "%param.browser%" "%param.host%" "%param.testsuite%" "%param.testsuite.results%" %param.options%
