5e3cc85841df6272dcc21e9ce01f1c77fd89e515
Emanuel Pedro Loureiro Teixeira <emanuel.teixeira@webdetails.pt>
2014-07-16 11:15:25 +0100

    Update timeout

--- run-html-tests.sh
+++ run-html-tests.sh
@@ -7,6 +7,6 @@ param_browser='*firefox'
 param_host='http://localhost:8080/pentaho/'
 param_testsuite='src-html\test_suite_global_jenkins.html'
 param_testsuite_results='results.html'
-param_options='-timeout 60000 -browserTimeout 60000'
+param_options='-timeout=60 -browserTimeout=60'
 
-java -jar ${param_lib_selenium_standalone} -htmlSuite "${param_browser}" "${param_host}" "${param_testsuite}" "${param_testsuite_results}" ${param_options}
+java -jar ${param_lib_selenium_standalone} ${param_options} -htmlSuite "${param_browser}" "${param_host}" "${param_testsuite}" "${param_testsuite_results}"
