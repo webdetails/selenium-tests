b5328275238ee8602608cd2d6457bc2ece2dc3fe
Emanuel Pedro Loureiro Teixeira <emanuel.teixeira@webdetails.pt>
2014-07-16 12:06:24 +0100

    Remove timeout, doesn't work through command line. Correct the script.

--- run-html-tests.bat
+++ run-html-tests.bat
@@ -5,6 +5,6 @@ set param.browser=*firefox
 set param.host=http://localhost:8080/pentaho/
 set param.testsuite=src-html\test_suite_global_jenkins.html
 set param.testsuite.results=results.html
-set param.options=-timeout=60 -browserTimeout=60
+set param.options=
 
 java -jar %param.lib.selenium.standalone% %param.options% -htmlSuite "%param.browser%" "%param.host%" "%param.testsuite%" "%param.testsuite.results%"
