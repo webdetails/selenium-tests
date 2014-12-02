b63ae951e605f8d8e983be3a28c4ebced1a60677
Eduardo Lima Lopes <eduardo.lopes@webdetails.pt>
2014-12-02 17:25:48 +0000

    Update test suite opendemos

--- run-html-tests.bat
+++ run-html-tests.bat
@@ -3,7 +3,7 @@ echo ON
 set param.lib.selenium.standalone=lib\selenium-server-standalone-2.44.0.jar
 set param.browser=*firefox C:\Program Files (x86)\Mozilla Firefox\firefox.exe
 set param.host=http://localhost:8080/pentaho/
-set param.testsuite=src-html\test_suite_cde_tutorial.html
+set param.testsuite=src-html\test_suite_opendemos.html
 set param.testsuite.results=results.html
 set param.options=
 
