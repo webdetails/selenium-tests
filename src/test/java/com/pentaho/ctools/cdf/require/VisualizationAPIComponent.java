23776c0e271ad07a879393169de4ba22cc0eda13
PENTAHO\rsarreira <Ronye1981>
2017-07-17 16:40:49 +0100

    [FIX] Changed values in "ExpectedValue" --> Max / Min / Avg VisualizationAPIComponet.java

--- src/test/java/com/pentaho/ctools/cdf/require/VisualizationAPIComponent.java
+++ src/test/java/com/pentaho/ctools/cdf/require/VisualizationAPIComponent.java
@@ -142,7 +142,7 @@ public class VisualizationAPIComponent extends BaseTest {
 		/*
 		 * ## Step 1
 		 */
-		String expectedValue = "The result is 35659";
+		String expectedValue = "The result is 35659.00";
 		String actualValue = this.elemHelper.WaitForTextPresence( driver, By.cssSelector( "#sampleObject>span" ), expectedValue );
 
 		assertEquals( actualValue, expectedValue );
@@ -194,7 +194,7 @@ public class VisualizationAPIComponent extends BaseTest {
     this.elemHelper.WaitForElementPresenceAndVisible( driver, By.id( "sample" ) );
     this.elemHelper.WaitForElementPresenceAndVisible( driver, By.cssSelector( "#sampleObject>span" ), 5 );
 
-    String expectedValue = "The result is 490";
+    String expectedValue = "The result is 490.00";
     String actualValue = this.elemHelper.WaitForTextPresence( driver, By.cssSelector( "#sampleObject>span" ), expectedValue );
 
     assertEquals( expectedValue, actualValue );
@@ -231,19 +231,19 @@ public class VisualizationAPIComponent extends BaseTest {
 		this.elemHelper.SendKeys( driver, By.xpath( "//div[@id='code']/textarea" ), text );
 
 		// Click in Try me
-    this.elemHelper.ClickJS( driver, By.cssSelector( "#code > button" ) );
-    // NOTE - we have to wait for loading disappear
-    this.elemHelper.WaitForElementPresence( driver, By.cssSelector( "div.blockUI.blockOverlay" ), 5 );
-    this.elemHelper.WaitForElementInvisibility( driver, By.cssSelector( "div.blockUI.blockOverlay" ) );
-    // Now sample element must be displayed
-    assertTrue( this.elemHelper.FindElement( driver, By.id( "sample" ) ).isDisplayed() );
+		this.elemHelper.ClickJS( driver, By.cssSelector( "#code > button" ) );
+		// NOTE - we have to wait for loading disappear
+		this.elemHelper.WaitForElementPresence( driver, By.cssSelector( "div.blockUI.blockOverlay" ), 5 );
+		this.elemHelper.WaitForElementInvisibility( driver, By.cssSelector( "div.blockUI.blockOverlay" ) );
+		// Now sample element must be displayed
+		assertTrue( this.elemHelper.FindElement( driver, By.id( "sample" ) ).isDisplayed() );
 
 		/*
 		 * ## Step 2 - Check the presented value for MIN.
 		 */
 		this.elemHelper.WaitForElementPresenceAndVisible( driver, By.id( "sample" ) );
-        this.elemHelper.WaitForElementPresenceAndVisible( driver, By.cssSelector( "#sampleObject>span" ), 5 );
-		String expectedValue = "The result is 4787.772727272727";
+        this.elemHelper.WaitForElementPresenceAndVisible( driver, By.cssSelector( "#sampleObject>span" ) );
+		String expectedValue = "The result is 4787.77";
 		String actualValue = this.elemHelper.WaitForTextPresence( driver, By.cssSelector( "#sampleObject>span" ), expectedValue );
 		assertEquals( actualValue, expectedValue );
 	}
