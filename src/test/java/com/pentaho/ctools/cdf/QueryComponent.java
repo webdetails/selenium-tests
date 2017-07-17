aa07614c1db3d715c09acf50bbff7ce3470d9125
Emanuel Teixeira <eteixeira@pentaho.com>
2017-07-17 18:19:21 +0100

    [FIX] Correct script - now in require QueryComponent just count the number of element presences after reload sample (doesn't care about display value)

--- src/test/java/com/pentaho/ctools/cdf/QueryComponent.java
+++ src/test/java/com/pentaho/ctools/cdf/QueryComponent.java
@@ -24,12 +24,9 @@ package com.pentaho.ctools.cdf;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import com.pentaho.ctools.utils.ElementHelper;
@@ -126,8 +123,8 @@ public class QueryComponent extends BaseTest {
 
     //Check the number of divs with id 'SampleObjectResult'
     //Hence, we guarantee when click Try Me the previous div is replaced
-     int nSampleObject = this.elemHelper.FindElements( driver, By.cssSelector( "#sampleObjectResult" ) ).size();
-     assertEquals( 1, nSampleObject );
+    int nSampleObject = this.elemHelper.FindElements( driver, By.cssSelector( "#sampleObjectResult" ) ).size();
+    assertEquals( 1, nSampleObject );
   }
 
   /**
